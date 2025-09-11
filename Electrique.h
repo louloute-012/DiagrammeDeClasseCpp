c236b67bc2ee3fd567ac3d94d2a5569e2058bca4
louloute-012 <lou.ann91@icloud.com>
2025-09-11 09:21:57 +0200

    ajout du nouveau Electrique.h fonctionelle

--- Electrique.h
+++ Electrique.h
@@ -1,32 +1,19 @@
-* Cette classe a une finalité pédagogique :
-*\li : Rappel du C++
-* \li : Commentaire avec Doxygen
-* \li : Git, GitHub
-* /
-class Mission
-{
+#ifndef ELECTRIQUE_H
+#define ELECTRIQUE_H
+
+#include "Moteur.h"
+
+class Electrique : public Moteur {
 private:
-    int nbPersonne;                ///< Nombre de personnes affectées à la mission
-    std::string descriptifMission; ///< Description textuelle de la mission
-    int nbHeure;                   ///< Durée en heures de la mission
+    float tensionMax;
 
 public:
-    /* Forme canonique */
-    Mission();                                                        // Constructeur par défaut
-    Mission(int nbPersonne, const std::string& descriptif, int nbHeure); // Constructeur avec paramètres
-    Mission(const Mission& m);                                        // Constructeur de copie
-    Mission& operator=(const Mission& m);                             // Opérateur d’affectation
-    ~Mission();                                                       // Destructeur
+    Electrique(int p = 0, float tension = 0.0);
 
-    /* Accesseurs / Mutateurs */
-    int getNbPersonne() const;
-    void setNbPersonne(int nb);
+    float getTensionMax() const;
+    void setTensionMax(float tension);
 
-    std::string getDescriptifMission() const;
-    void setDescriptifMission(const std::string& desc);
-
-    int getNbHeure() const;
-    void setNbHeure(int heures);
+    void afficher() const override;
 };
 
-#endif // MISSION_H
+#endif
