f73f63d1715165d701bf10f07d7d13a9541204f7
louloute-012 <lou.ann91@icloud.com>
2025-09-11 09:13:55 +0200

    ajout du nouveau moteur.h fonctionnelle et qui est reliée a moteur.cpp et moto

--- moteur.h
+++ moteur.h
@@ -1,50 +1,18 @@
-/*****************************************************************//**
- * \file   Mission.h
- * \brief  Déclaration de la classe Mission
- *
- * \author Pierre
- * \date   June 2023
- *********************************************************************/
+#ifndef MOTEUR_H
+#define MOTEUR_H
 
-#ifndef MISSION_H
-#define MISSION_H
-
-#include <iostream>
-#include <string>
-
- /*!
-  * \class Mission
-  * \brief Classe représentant une mission (nombre de personnes, descriptif, durée en heures)
-  *
-  * Cette classe a une finalité pédagogique :
-  * \li : Rappel du C++
-  * \li : Commentaire avec Doxygen
-  * \li : Git, GitHub
-  */
-class Mission
-{
-private:
-    int nbPersonne;                ///< Nombre de personnes affectées à la mission
-    std::string descriptifMission; ///< Description textuelle de la mission
-    int nbHeure;                   ///< Durée en heures de la mission
+class Moteur {
+protected:
+    int puissance;
 
 public:
-    /* Forme canonique */
-    Mission();                                                        // Constructeur par défaut
-    Mission(int nbPersonne, const std::string& descriptif, int nbHeure); // Constructeur avec paramètres
-    Mission(const Mission& m);                                        // Constructeur de copie
-    Mission& operator=(const Mission& m);                             // Opérateur d’affectation
-    ~Mission();                                                       // Destructeur
+    Moteur(int p = 0);
 
-    /* Accesseurs / Mutateurs */
-    int getNbPersonne() const;
-    void setNbPersonne(int nb);
+    int getPuissance() const;
+    void setPuissance(int p);
 
-    std::string getDescriptifMission() const;
-    void setDescriptifMission(const std::string& desc);
-
-    int getNbHeure() const;
-    void setNbHeure(int heures);
+    virtual void afficher() const;
+    virtual ~Moteur() {}
 };
 
-#endif // MISSION_H
+#endif
