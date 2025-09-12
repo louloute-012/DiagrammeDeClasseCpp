d21fb1e8fb61daf64226c849b72a3a840aab8d45
louloute-012 <lou.ann91@icloud.com>
2025-09-12 09:02:39 +0200

    ajout de electrique.cpp final et annoter

--- Electrique.cpp
+++ Electrique.cpp
@@ -2,12 +2,24 @@
 #include <iostream>
 using namespace std;
 
+
+
+// Constructeur : initialise le moteur électrique
+// Appelle le constructeur de la classe mère Moteur
+// puis initialise l'attribut spécifique 'tensionMax'
 Electrique::Electrique(int p, float tension) : Moteur(p), tensionMax(tension) {}
 
+
+// Renvoie la tension maximale
 float Electrique::getTensionMax() const { return tensionMax; }
+
+// Modifie la tension maximale
 void Electrique::setTensionMax(float tension) { tensionMax = tension; }
 
+// Redéfinit la méthode virtuelle afficher() de Moteur
+// Affiche les infos spécifiques au moteur électrique
 void Electrique::afficher() const {
     cout << "Moteur Electrique -> Puissance: " << puissance
         << " ch, Tension max: " << tensionMax << " V" << endl;
 }
+
